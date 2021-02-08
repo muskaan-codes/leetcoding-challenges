74815ead680f7f7c17f5fbffb2eaa4fa954a92de
Muskaan Manocha <70715304+muskaan-codes@users.noreply.github.com>
2021-02-08 13:53:45 +0530

    Create Day-3-Linked-List-Cycle.py

---
+++ FebruaryCodingChallenge/Day-3-Linked-List-Cycle.py
@@ -0,0 +1,18 @@
+# Definition for singly-linked list.
+# class ListNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
+class Solution(object):
+    def hasCycle(self, head):
+        """
+        :type head: ListNode
+        :rtype: bool
+        """
+        a = b = head
+        while b and b.next:
+            a = a.next
+            b = b.next.next
+            if a == b: return True  # floyd's algorithm for cycle detection
+        return False
